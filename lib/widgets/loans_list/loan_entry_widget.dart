e56e1db9afb3f5c8d98eaf904fe0bdf9159a53c0
pgolez <paul.golez@codingavenue.com>
2020-08-16 20:03:47 +0800

    Refine loan entry widget

--- lib/widgets/loans_list/loan_entry_widget.dart
+++ lib/widgets/loans_list/loan_entry_widget.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 
 class LoanEntryWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
-        leading: CircleAvatar(child: Text("MZ"),),
+        leading: CircleAvatar(
+          child: Text("MZ"),
+        ),
         title: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Text("Marjorie Zabala"),
-            Text("Next payment: November 20, 2020")
+            LinearPercentIndicator(
+              percent: 0.6,
+              animation: true,
+              animationDuration: 600,
+              padding: EdgeInsets.all(0),
+              progressColor: Theme.of(context).primaryColor,
+            ),
           ],
+        ),
+        subtitle: Text.rich(
+          TextSpan(
+            children: [
+              const TextSpan(text: 'Next payment: '),
+              TextSpan(
+                text: 'August 20, 2020',
+                style: TextStyle(fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
         ),
         trailing: Icon(Icons.chevron_right),
       ),
